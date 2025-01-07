4024f45c7a24d254745af5ca5fb7b040acc03c04
Dylpickle2008 <dylanrigby1016@gmail.com>
2025-01-07 03:00:39 +0000

    bug fixes 1

--- indoor.py
+++ indoor.py
@@ -2,7 +2,8 @@ def main():
     loudest = input("SCREAM in your loudest capital voice!!! ").strip().casefold()
     lower(loudest)
 
-def lower():
+
+def lower(loudest):
     print(f"Be quiet, this is a library bro, {loudest}")
 
 main()
